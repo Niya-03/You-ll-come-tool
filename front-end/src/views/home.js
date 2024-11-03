4a386f586bd8303ede5cb33aa8b3bf989132d188
Niya-03 <niya.dimitrova.2016@gmail.com>
2024-11-03 13:14:33 +0200

    Fixed home page

--- front-end/src/views/home.js
+++ front-end/src/views/home.js
@@ -2,8 +2,21 @@ import {html, render} from '../lib.js';
 
 const homeTemplate = () => html `
     <section id="home">
-            <h1>Welcome to You'll come tool. The app to use to make your friends go to budget trips.</h1>
-        </section>
+    <div class="intro-block">
+        <h1>Ready for your next trip?</h1>
+    </div>
+</section>
+
+<section id="airplane-section">
+    <div class="parallax-background"></div>
+    <div class="airplane-content">
+        <div class="text-content">
+            <h2>Are you sick of sending pictures of pages of trip planning? But how much is the plane ticket? Where will we stay?</h2>
+            <p>I am sick too. Now you can send one link with all the information and make your friends come too</p>
+        </div>
+        <a href="/alltrips"><button class="check-trips-btn" href="/alltrips">See all trips</button></a>
+    </div>
+</section>
 `;
 
 export function showHomeView(ctx){
